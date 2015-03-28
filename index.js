11829f9a175ee4e43f74403ea312dd4e881977ec
Doug Beck <doug@douglasbeck.com>
2015-03-28 15:01:43 -0400

    Add some tests

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-'use strict'
+'use strict';
 
-var cp = require('child_process')
-var fs = require('fs')
+var cp = require('child_process');
 var path = require('path');
 
 module.exports = function(ours, base, theirs, options) {
--- test/test.js
+++ test/test.js
@@ -3,22 +3,57 @@ var assert = require('assert');
 var path = require('path');
 var fs = require('fs');
 
-var pomerge = require('../index')
+var pomerge = require('../index');
 
-describe('merging', function() {
+function isDir(p) {
+  return fs.statSync(path.join(__dirname, p)).isDirectory();
+}
 
-  var ourPo;
-  before(function() {
-    ourPo = fs.readFileSync(path.join(__dirname, 'fixture', 'ours.po'));
+function getTestDirs() {
+  return fs.readdirSync(__dirname).filter(isDir);
+}
+
+describe('git-po-merge', function() {
+
+  var restore = [];
+  var save = function(files) {
+    var safeFile = function(f) {
+      restore.push({path:f, content: fs.readFileSync(f).toString()});
+    };
+    files.map(safeFile);
+  };
+  afterEach(function() {
+    var restoreFile = function (f) {
+      fs.writeFileSync(f.path, f.content);
+    };
+    restore.map(restoreFile);
   });
 
-  it('has a test', function() {
-    //TODO: write some real tests
-    var ours = path.join(__dirname, 'fixture', 'ours.po');
-    var base = path.join(__dirname, 'fixture', 'base.po');
-    var theirs = path.join(__dirname, 'fixture', 'theirs.po');
-    pomerge(ours, base, theirs, {silent: true});
-    assert.ok(true);
+  getTestDirs().map(function(dir) {
+
+    it(dir, function() {
+      var ours = path.join(__dirname, dir, 'ours.po');
+      var base = path.join(__dirname, dir, 'base.po');
+      var theirs = path.join(__dirname, dir, 'theirs.po');
+      var expected = path.join(__dirname, dir, 'expected.po');
+
+      save([ours, base, theirs]);
+
+      var shouldFail = dir.indexOf('fail') > 0;
+      var failed = false;
+      try {
+        pomerge(ours, base, theirs, {silent: true});
+      } catch(e) {
+        failed = true;
+      }
+      var merged = fs.readFileSync(ours).toString();
+      expected = fs.readFileSync(expected).toString();
+
+      assert.equal(shouldFail, failed);
+      assert.equal(merged, expected);
+
+    });
+
   });
 
 });
